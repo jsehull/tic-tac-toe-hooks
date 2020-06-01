cda1bb5285df8fbbb3d53686f4e900eb59529bfa
Jesse Hull <jse.hull@gmail.com>
2020-06-01 10:17:35 -0600

    setup board and square

---
+++ src/components/Game.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const styles = {
+  margin: '20px auto',
+  width: '200px'
+}
+
+const Game = () => {
+  return (
+    <div>Game</div>
+  )
+}
+
+export default Game
